9ce6310f3c3c9d5bd122a2c3137d599cb6646d87
gabrielkumar <64897211+gabrielkumar@users.noreply.github.com>
2023-12-01 10:52:39 +0100

    edit app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('/template/index.html')
 
 @app.route('/download_pdf', methods=['GET', 'POST'])
 def handle_pdf_download():
