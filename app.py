c183db1591d909b126c60dde58350b77b1f46f13
gabrielkumar <64897211+gabrielkumar@users.noreply.github.com>
2023-12-01 11:05:03 +0100

    change app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, send_file
-from pdf_generator import generate_pdf
+from flask import Flask, render_template
 from pdf_generator import download_pdf
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('/template/index.html')
+    return render_template('index.html')
 
 @app.route('/download_pdf', methods=['GET', 'POST'])
 def handle_pdf_download():
