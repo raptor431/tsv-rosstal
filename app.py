8482377ad69a77e76cfa23c9c00601505ffa53c2
happyplant154 <100048958+happyplant154@users.noreply.github.com>
2023-12-03 10:25:42 +0100

    integrate recaptcha v3 into flask app
    Einfache implementierung von recaptcha v3 https://developers.google.com/recaptcha/docs/v3?hl=de
    Zusätzlich implementierung von Recaptcha als Field für evtl. Verwendung innerhalb einer form

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request, abort
+from urllib import response
+import requests
+import secrets
 
+#app.py Flask Konstruktor aufrufen
 app = Flask(__name__)
 
+#app setup
+foo = secrets.token_urlsafe(16)
+app.secret_key = foo
+app.static_folder = 'static'
 
+#für site key und secret key frag entweder Philipp oder hol dir deine eigenen auf https://www.google.com/recaptcha/admin/create?hl=de 
+#!!! V3 RECAPTCHA !!!#
+SITE_KEY = 'YOUR_SITE_KEY'
+SECRET_KEY = 'YOUR_SECRET_KEY'
+VERIFY_URL = 'https://www.google.com/recaptcha/api/siteverify'
+
+
+#views
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/mitglied-werden')
+def mitglied_werden():
+    return render_template('mitglied-werden.html')
 
+@app.route('/mitgliedsantrag', methods=["GET", "POST"])
+def mitgliedsantrag():
+    if request.method == 'POST':        
+        secret_response = request.form['g-recaptcha-response']
+        verify_response = requests.post(url=f'{VERIFY_URL}?secret={SECRET_KEY}&response={secret_response}').json()
+        if verify_response['success'] == False or verify_response['score'] < 0.7:
+            abort(401)#if bot detected or recaptcha request failed
+        else: 
+            return render_template('daten-uebermittelt.html')
+    else:
+        return render_template('mitgliedsantrag.html', site_key=SITE_KEY)
+
+
+#mainloop
 if __name__ == '__main__':
     app.run(debug=True)
